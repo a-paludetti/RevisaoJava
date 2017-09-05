01e675a0bf06abd2016ae1b2377b697863671cad
aline.poliveira3 <aline.poliveira3@CAS576156W1032.senacsp.edu.br>
2017-09-05 20:35:40 -0300

    Lidando com objetos (criado um contador!)

---
+++ src/exercicio2_05092017/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio2_05092017;
+
+import exercicio2_05092017.Contador;
+
+public class Main {
+    public static void main(String[] args) {
+       Contador C = new Contador();
+      
+       C.valorAtual();
+       
+    }
+}
