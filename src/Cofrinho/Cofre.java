da7b848c8206004811fe3730d55beab70469d43d
aline.poliveira3 <aline.poliveira3@CAS576156W1032.senacsp.edu.br>
2017-09-12 21:55:12 -0300

    exercicio cofre

---
+++ src/Cofrinho/Cofre.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cofrinho;
+
+import Cofrinho.Moeda;
+
+/**
+ *
+ * @author aline.poliveira3
+ */
+public class Cofre {
+
+    private ArrayList<Moeda> moedas;
+
+    public Cofre() {
+        moedas = new ArrayList<>();
+    }
+}
---
+++ src/Cofrinho/Moeda.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cofrinho;
+
+/**
+ *
+ * @author aline.poliveira3
+ */
+public class Moeda {
+
+    private float valor;
+
+    public float getMoeda() {
+        return valor;
+    }
+
+}
